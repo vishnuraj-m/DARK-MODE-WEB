cef4343195bee380deea0be0752c681aaf541859
vishnuacappella <acappella.studiov@gmail.com>
2022-08-03 19:46:23 +0530

    dark mode demo

---
+++ app.js
@@ -0,0 +1,12 @@
+const body = document.querySelector('body');
+
+const changeMode = () => {
+    // if (!body.classList.contains('dark')) {
+    //     body.classList.add('dark')
+    // } else {
+    //     body.classList.remove('dark')
+    // }
+
+
+    body.classList.toggle('dark');
+}
